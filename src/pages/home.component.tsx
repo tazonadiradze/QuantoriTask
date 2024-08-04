a171c818c01f741025e3952f2ce53f24759f8c88
tazo01~ <nadiradzetazo123@gmail.com>
2024-08-04 18:34:14 +0400

    finish home page

--- src/pages/home.component.tsx
+++ src/pages/home.component.tsx
@@ -1,21 +1,11 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[2fr_2fr_2fr_2fr] grid-cols-[2fr_2fr_2fr] h-[100%]">
-      <div className="col-start-1 col-end-2 row-start-1 row-end-4 bg-green-500">
-        green
-      </div>
-      <div className="col-start-2 col-end-4 row-start-1 row-end-1 bg-red-500">
-        brown
-      </div>
-      <div className="col-start-2 col-end-3 row-start-2 row-end-4 bg-black">
-        dark green
-      </div>
-      <div className="col-start-3 col-end-3 row-start-2 row-end-5 bg-orange-500">
-        orange
-      </div>
-      <div className="col-start-1 col-end-3 row-start-4 row-end-4 bg-blue-500">
-        bliue
-      </div>
+    <div className="grid grid-rows-[2fr_2fr_2fr_2fr] grid-cols-[2fr_2fr_2fr] h-[100%] relative ">
+      <div className="col-start-1 col-end-2 row-start-1 row-end-4 bg-emerald-700"></div>
+      <div className="col-start-2 col-end-4 row-start-1 row-end-1 bg-orange-700"></div>
+      <div className="col-start-2 col-end-3 row-start-2 row-end-4 bg-green-950"></div>
+      <div className="col-start-3 col-end-3 row-start-2 row-end-5 bg-orange-500"></div>
+      <div className="col-start-1 col-end-3 row-start-4 row-end-4 bg-violet-300"></div>
     </div>
   );
 }
--- src/pages/log-in.component.tsx
+++ src/pages/log-in.component.tsx
@@ -2,13 +2,48 @@ import { useLoginContext } from "../context/login.context";
 
 const LogIn = () => {
   const { showLogin } = useLoginContext();
-
   if (!showLogin) {
     return null;
   }
 
   return (
-    <div className="flex justify-center items-center">hello this is log in</div>
+    <div className="flex justify-center items-center text-2xl text-green-500 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 ">
+      <div className="border-4  bg-white ">
+        <h2 className="text-2xl font-bold text-gray-800 mb-6">Login</h2>
+        <form>
+          <div className="mb-4">
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              className="w-full p-2 border border-gray-300 rounded mt-1"
+            />
+          </div>
+          <div className="mb-6">
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              className="w-full p-2 border border-gray-300 rounded mt-1"
+            />
+          </div>
+          <div className="flex justify-between">
+            <button
+              type="button"
+              className="px-4 py-2 bg-gray-300 text-gray-700 rounded"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="px-4 py-2 bg-green-500 text-white rounded"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
